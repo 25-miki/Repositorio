298aabfc99e559add9f36341acef0841e0edfaa1
25-miki <marmarllo16@alu.edu.gva.es>
2024-09-26 17:05:36 +0200

    ..

--- tema2/funciones/ficheros/plantillas.php
+++ tema2/funciones/ficheros/plantillas.php
@@ -7,16 +7,18 @@ while (!feof($fp)){
     $linea = fgets($fp);
     $lista[] = explode(',', $linea);
 }
+print_r($lista);
 
 $header = array_shift($lista);
 foreach ($lista as $ls){
     $tabla[] = array_combine($header, $ls);
 }
 
+
 fclose($fp);
 
 
-include 'plantilla_view.php';
+//include 'plantilla_view.php';
 
 
 ?>
