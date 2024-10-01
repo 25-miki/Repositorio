541bf2172eb51b6f8e1d72d4380c424ef3ab83f4
25-miki <marmarllo16@alu.edu.gva.es>
2024-10-01 20:59:21 +0200

    ..

---
+++ tema3/calculadora.view.php
@@ -0,0 +1,13 @@
+
+
+<h2>Resultados:</h2>
+<p>Suma: <?php echo "$x + $y = $suma"; ?></p>
+<p>Resta: <?php echo "$x - $y = $resta"; ?></p>
+<p>Multiplicación: <?php echo "$x * $y = $multiplicacion"; ?></p>
+<p>División: <?php echo $division; ?></p>
+
+<h2>Valores de la variable $_GET:</h2>
+<pre><?php print_r($_GET); ?></pre>
+
+<h2>Valores de la variable $_SERVER:</h2>
+<pre><?php print_r($_SERVER); ?></pre>
--- tema3/formulario.php
+++ tema3/formulario.php
@@ -1,66 +1,67 @@
 
+<?php
+include "header.php";
+?>
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $nombre = $_POST['nombre'];
+    $email = $_POST['email'];
+    $pagina = $_POST['pagina'];
+    $sexo = $_POST['sexo'];
+    $convivientes = $_POST['convivientes'];
 
-<?php if ($_SERVER["REQUEST_METHOD"] == "POST"):  
+    if (!empty($_POST['aficiones'])) {
+        $aficiones = implode(", ", $_POST['aficiones']);
+    } else {
+        $aficiones = "Ninguna afición seleccionada";
+    }
 
-$nombre = $_POST['nombre'];
-$email = $_POST['email'];
-$pagina = $_POST['pagina'];
-$sexo = $_POST['sexo'];
-$convivientes = $_POST['convivientes'];
+    if (!empty($_POST['menu'])) {
+        $menu = implode(", ", $_POST['menu']);
+    } else {
+        $menu = "Ningún menú seleccionado";
+    }
 
-if (!empty($_POST['aficiones'])) {
-    $aficiones = implode(", ", $_POST['aficiones']);
-} else {
-    $aficiones = "Ninguna afición seleccionada";
+    echo '
+    <h2>Resumen de Datos</h2>
+    <table class="table table-bordered">
+        <thead>
+            <tr>
+                <th>Campo</th>
+                <th>Valor</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <td>Nombre y Apellidos</td>
+                <td>' . $nombre . '</td>
+            </tr>
+            <tr>
+                <td>Email</td>
+                <td>' . $email . '</td>
+            </tr>
+            <tr>
+                <td>URL Página Personal</td>
+                <td>' . $pagina . '</td>
+            </tr>
+            <tr>
+                <td>Sexo</td>
+                <td>' . $sexo . '</td>
+            </tr>
+            <tr>
+                <td>Número de Convivientes</td>
+                <td>' . $convivientes . '</td>
+            </tr>
+            <tr>
+                <td>Aficiones</td>
+                <td>' . $aficiones . '</td>
+            </tr>
+            <tr>
+                <td>Menú Favorito</td>
+                <td>' . $menu . '</td>
+            </tr>
+        </tbody>
+    </table>';
 }
 
-if (!empty($_POST['menu'])) {
-    $menu = implode(", ", $_POST['menu']);
-} else {
-    $menu = "Ningún menú seleccionado";
-}
-    
-echo '
-<h2>Resumen de Datos</h2>
-<table class="table table-bordered">
-    <thead>
-        <tr>
-            <th>Campo</th>
-            <th>Valor</th>
-        </tr>
-    </thead>
-    <tbody>
-        <tr>
-            <td>Nombre y Apellidos</td>
-            <td>' . $nombre . '</td>
-        </tr>
-        <tr>
-            <td>Email</td>
-            <td>' . $email . '</td>
-        </tr>
-        <tr>
-            <td>URL Página Personal</td>
-            <td>' . $pagina . '</td>
-        </tr>
-        <tr>
-            <td>Sexo</td>
-            <td>' . $sexo . '</td>
-        </tr>
-        <tr>
-            <td>Número de Convivientes</td>
-            <td>' . $convivientes . '</td>
-        </tr>
-        <tr>
-            <td>Aficiones</td>
-            <td>' . $aficiones . '</td>
-        </tr>
-        <tr>
-            <td>Menú Favorito</td>
-            <td>' . $menu . '</td>
-        </tr>
-    </tbody>
-</table>';
-    
-    
-    ?>
-
+?>
