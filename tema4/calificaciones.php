1de2c08269e390db95db5a5033f2e6b6412f23d7
25-miki <marmarllo16@alu.edu.gva.es>
2024-10-24 16:19:35 +0200

    Update calificaciones.php

--- tema4/calificaciones.php
+++ tema4/calificaciones.php
@@ -1,42 +1,24 @@
 <!DOCTYPE html>
 <html lang="es">
-
 <head>
-    <?php
-    include "../header.php";
-    ?>
+    <title>Calificaciones</title>
+    <?php include "header.php"; ?>
 </head>
-
 <body>
-    <div style="width: 50vw; padding: 2rem">
+    <div style="width: 70vw; padding: 2rem">
         <h2>Calificaciones</h2>
         <form action="calificaciones.php" method="POST" class="container mt-5">
-            <label for="nombre">Nombre de alumno</label>
-            <input type="text" name="nombre" id="nombre" required>
-            <br>
-            <label for="first">Nota primer trimestre</label>
-            <input type="number" name="first" id="first" required>
-            <br>
-            <label for="sec">Nota segundo trimestre</label>
-            <input type="number" name="sec" id="sec" required>
-            <br>
-            <label for="third">Nota tercer trimestre</label>
-            <input type="number" name="third" id="third" required>
-            <br>
-            <button type="submit" class="btn btn-primary">Agregar</button>
+            <input type="text" name="nombre" placeholder="Nombre de alumno" required>
+            <input type="number" name="first" placeholder="Nota primer trimestre" required>
+            <input type="number" name="sec" placeholder="Nota segundo trimestre" required>
+            <input type="number" name="third" placeholder="Nota tercer trimestre" required>
+            <button type="submit" class="btn btn-primary">Enviar</button>
         </form>
-    </div>
 
-    <table border="1" style="width: 100%; margin-top: 2rem">
-        <tr>
-            <th>Nombre</th>
-            <th>Nota 1º Trim</th>
-            <th>Nota 2º Trim</th>
-            <th>Nota 3º Trim</th>
-            <th>Media</th>
-        </tr>
-        <?php
+        <?php 
         session_start();
+
+        include "header.php";
 
         if (!isset($_SESSION['notas'])) {
             $_SESSION['notas'] = [];
@@ -56,17 +38,40 @@
             ];
         }
 
-        foreach ($_SESSION['notas'] as $alumno):
-        ?>
-            <tr>
-                <td><?php echo htmlspecialchars($alumno['nombre']); ?></td>
-                <td><?php echo $alumno['first']; ?></td>
-                <td><?php echo $alumno['sec']; ?></td>
-                <td><?php echo $alumno['third']; ?></td>
-                <td><?php echo round(($alumno['first'] + $alumno['sec'] + $alumno['third']) / 3, 1); ?></td>
-            </tr>
-        <?php endforeach; ?>
-    </table>
-</body>
+        if (isset($_GET['accion']) && $_GET['accion'] == 'vaciar') {
+            $_SESSION['notas'] = array();
+        }
 
+        // Displaying the table only if there are notes
+        if (!empty($_SESSION['notas'])): ?>
+            <table border="1" style="margin-top: 2rem;">
+                <tr>
+                    <th>Nombre</th>
+                    <th>Nota 1º trim</th>
+                    <th>Nota 2º trim</th>
+                    <th>Nota 3º trim</th>
+                    <th>Media</th>
+                </tr>
+                <?php foreach ($_SESSION['notas'] as $alumno): ?>
+                    <tr>
+                        <td><?php echo htmlspecialchars($alumno['nombre']); ?></td>
+                        <td><?php echo $alumno['first']; ?></td>
+                        <td><?php echo $alumno['sec']; ?></td>
+                        <td><?php echo $alumno['third']; ?></td>
+                        <td><?php echo round(($alumno['first'] + $alumno['sec'] + $alumno['third']) / 3, 2); ?></td>
+                    </tr>
+                <?php endforeach; ?>
+
+                
+
+
+                
+
+            </table>
+            <div>
+                <a href="calificaciones.php?accion=vaciar" class="btn btn-danger">Borrar</a>
+            </div>
+        <?php endif; ?>
+    </div>
+</body>
 </html>
